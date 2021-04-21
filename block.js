2e747414564cab9bc501098d3f4f3939a3dd1438
hks24 <74139697+hks24@users.noreply.github.com>
2021-04-21 19:00:28 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,26 @@
+class Block{
+    constructor(x,y,width,height,options){
+       var options = {
+       isStatic : false,
+       restitution : 0.3,
+       density : 10
+}
+this.body = Bodies.rectangle(x,y,width,height,options);
+this.width = width;
+this.height = height;
+World.add(world,this.body);
+
+
+}
+display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x,pos.y);
+    stroke("black");
+    fill("white");
+    rectMode(CENTER);
+    rect(0,0,this.width,this.height);
+    pop();
+
+}
+}
